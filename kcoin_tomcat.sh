525de13d8140027bb8af3708b21930a79e046bfc
haofeann <haofeann@me.com>
2019-07-05 21:00:05 +0800

    update

--- kcoin_tomcat.sh
+++ kcoin_tomcat.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 mkdir -p /data/kcoin
 mkdir -p /data/logs/app/
-
+ln -s /data/kcoin /usr/local/kcoin
 tar -xvf /tmp/apache-tomcat-8.5.29.tar.gz -C /usr/local/kcoin/
 rm -rf /usr/local/kcoin/apache-tomcat-8.5.29/webapps/*
 mv /tmp/*.war /usr/local/kcoin/apache-tomcat-8.5.29/webapps/
+mv /tmp/config.properties /usr/local/kcoin/apache-tomcat-8.5.29/conf
+mv /tmp/server.xml /usr/local/kcoin/apache-tomcat-8.5.29/conf -f
